e86d0586a107d65b617dca3f619e70eb7d975ccb
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-04 14:18:53 -0500

    Understanding what to do

--- app.js
+++ app.js
@@ -12,3 +12,13 @@ let gameState = {
 
 
 // What Game are we making.  Tik Tak Toe or COnnect 4?
+// Creating multiple HTML elements all at the same time
+    //Grab the element where you want to store all of those new elements
+    // so try a container
+    let listContainerEle = document.getElementsByClassName("container-ele");
+
+// Step 2) Lets us a loop to make a bunch of elemeents at once.
+for (let numOfElementsToCrate = 1; numOfElementsToCrate<= 20; numOfElementsToCrate++) {
+    let newElement = document.createElement("li");
+    console.log("Current element we are on: $(numOfElementsToCreate)")
+};
