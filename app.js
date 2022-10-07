9af37cbe1c2e7a5197d6636678b5fae8487a4361
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-07 15:45:52 -0500

    i like this

--- app.js
+++ app.js
@@ -13,28 +13,7 @@
     var playerTwo = "two";
     var currentPlayer = playerOne;    
 
-// When the Page Loads
-window.onload = function(){
-    loadBoard();
-}
 
-function loadBoard  () {
-    board = [];
-    for (let  i = 0; i < rows; i++){
-        let row = [];
-        for (let j = 0; j < columns; j++){
-            row.push(" ");
-
-            let circle = document.createElement("div");
-            circle.id = i.toString() + "-" + j.toString();
-
-        }
- 
-
-        let tile = document.createElement("div");
-        tile.id = 
-    }
-}
 
 
 // Size of Connect 4 Board = 7 Columns, 6 Rows
