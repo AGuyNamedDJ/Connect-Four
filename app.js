3810d2487cd53948ce30285c5c4a8a2b2fc75e19
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-07 18:16:48 -0500

    updated player coordinates

--- app.js
+++ app.js
@@ -1,33 +1,57 @@
-// Game State
-    // Board
-    var Board;
-    var rows = 6:
-    var columns = 7;
-
-    // Game Status
-    var gameEnded = false;
-
-    // Players
-    var playerOne = "one";
-    var playerTwo = "two";
-    var currentPlayer = playerOne;    
-
-
 // Selectors
-let boardRow = document.getElementById("tr"); 
-let boardCell = document.getElementById("td");
+let boardRow = document.getElementsByClassName("tr"); 
+let boardCell = document.getElementsByClassName ("td");
 let boardCircle = document.querySelector(".circle");
-let currentPlayer = document.querySelector(".currentPlayer");
+let currentPlayer = document.querySelector(".player-turn");
 let clear = document.querySelector(".clear");
 
-// Get the location for what slot we click on.
- for (let i = 0; i < tableCell.length; i++) {
-    // So why i is less than the cells
-    tableCell[i].addEventListener("click", )
- }
+// Finding the Table Coordinates
+for (let i = 0; i < boardCell.length; i++){
+    // While i is less than the # of cells
+    boardCell[i].addEventListener("click", (e) =>{
+        // Adding event listener(click) for each cell
+        console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`);
+            // Row is X coordinate, Y coordinate will be the column
+    })
+}
+
+// Player Names
 
 
 
 
+function setBoard() {
+    if (gameEnded) {
+        return; 
+    } else {
+        let location = this
+    }
+    
+}
 
-// stopped at 114
+
+
+//  Scratch
+// // Get the location for what slot we click on.
+//  for (let i = 0; i < tableCell.length; i++) {
+//     // So why i is less than the cells
+//     tableCell[i].addEventListener("click", )
+//  }
+
+
+// Game State
+    // // Board
+    // var Board;
+    // var rows = 6:
+    // var columns = 7;
+
+    // // Game Status
+    // var gameEnded = false;
+
+    // // Players
+    // var playerOne = "one";
+    // var playerTwo = "two";
+    // var currentPlayer = playerOne;    
+
+
+
