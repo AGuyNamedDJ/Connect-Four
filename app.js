d6269238179fd2ee04e6071ea806532e0de254fe
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-08 18:05:12 -0500

    updating stopping point

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ while (!playerOne){
     let playerOne = prompt("Player One: What is your Name? ");
     // Grabs input from player & assigns it
 };
-var playerOneColor = 'red';
+var playerOneColor = "white";
     // Save this color for player
 
 while (!playerTwo){
     let playerTwo = prompt("Player Two: What is your name? ");
 };
-var playerTwoColor = 'yellow';
+var playerTwoColor = "brown";
 
-let tableRow = document.getElementsByTagName('tr');
-let tableData = document.getElementsByTagName('td');
-let playerTurn = document.querySelector('.player-turn');
-const slots = document.querySelectorAll('.slot');
-const reset = document.querySelector('.reset');
+let tableRow = document.getElementsByTagName("tr");
+let tableData = document.getElementsByTagName("td");
+let playerTurn = document.querySelector(".player-turn");
+const slots = document.querySelectorAll(".slot");
+const clear = document.querySelector(".clear");
 
 // Setting the player at the start of game
 let currentPlayer = 1;
@@ -40,24 +40,24 @@ function changeColor(e){
 
     for (i = 5; i > -1; i--){
         //Starting from the bottom so we go negative(-)
-        if (tableRow[i].children[column].style.backgroundColor == 'white'){
+        if (tableRow[i].children[column].style.backgroundColor == "white"){
             // So if the cell is white (unused) we can change the color
             row.push(tableRow[i].children[column]);
             if (currentPlayer === 1){
-                row[0].style.backgroundColor = 'red';
+                row[0].style.backgroundColor = "white";
                     // playerOnes Color is put in the index
                 }
             }else{
                 // Else is for playerTwo
-                row[0].style.backgroundColor = 'yellow';
+                row[0].style.backgroundColor = "brown";
             }
         }
     }
 
 Array.prototype.forEach.call(tableData, (cell) => {
-    cell.addEventListener('click', changeColor);
+    cell.addEventListener("click", changeColor);
     // Set all slots to white for new game.
-    cell.style.backgroundColor = 'white';
+    cell.style.backgroundColor = "white";
 });
 
 // Functions for Checking for a Winner
@@ -65,5 +65,5 @@ Array.prototype.forEach.call(tableData, (cell) => {
 function matchingColors(one, two, three, four){
     // Are there 4 colors in a row
     // this will be used in the other functions
-    return (one === two && one === three && one === four && one !== 'white' && one !== undefined);
+    return (one === two && one === three && one === four && one !== "white" && one !== undefined);
 }
