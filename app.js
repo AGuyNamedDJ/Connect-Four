6de2d8371ce5797613da14e6aa19f57fd86852ff
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-08 16:08:32 -0500

    back to previous spot

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Get Player Names & Save
 while (!playerOne){
     let playerOne = prompt("Player One: What is your Name? ");
+    // Grabs input from player & assigns it
 };
 var playerOneColor = 'red';
     // Save this color for player
@@ -22,4 +23,14 @@ var currentPlayer = 1;
 let winner;
 playerTurn.textContent = `${player1}'s turn!`
 
+// Log cell coordinates when clicked
 
+// Finding the Table Coordinates
+for (let i = 0; i < boardCell.length; i++){
+    // While i is less than the total # of cells
+    boardCell[i].addEventListener("click", (e) =>{
+        // Adding event listener(click) for each cell
+        console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`);
+            // Row is X coordinate, Y coordinate will be the column
+    })
+};
